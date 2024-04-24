f8ab9c711387e8fbd94a33d36dd820285f437b84
chidambardesai <chidambar.desai100@gmail.com>
2024-04-24 20:08:58 +0530

    Pocketnotes

---
+++ pocketnotes/src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from '../components/SideBarAndMessageArea';
+
+const Home = () => {
+    return (
+        <div style={{ display: 'flex' }}>
+            <Homepage />
+        </div>
+    );
+};
+
+export default Home;
